2a0a865029d37f266a95101dc18cee0f41a1a87d
SeHyun <82575498+sehyun79@users.noreply.github.com>
2022-12-19 17:11:29 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,71 +1,39 @@
 import "./styles.css";
 
 var rolLength = 6;
-var setNum;
-var hiddenInput = document.createElement("input");
-hiddenInput.className = "hidden-input";
 
 const rRandom = () => {
   var min = Math.ceil(0);
   var max = Math.floor(rolLength - 1);
-  return Math.floor(Math.random() * (max - min)) + min;
+  return 3;
 };
 
 const rRotate = () => {
   var panel = document.querySelector(".rouletter-wacu");
-  var btn = document.querySelector(".rouletter-btn");
   var deg = [];
   for (var i = 1, len = rolLength; i <= len; i++) {
     deg.push((360 / len) * i);
   }
 
-  var num = 0;
-  document.body.append(hiddenInput);
-  setNum = hiddenInput.value = rRandom();
-
-  var ani = setInterval(() => {
-    num++;
-    panel.style.transform = "rotate(" + 360 * num + "deg)";
-    btn.disabled = true; //button,input
-    btn.style.pointerEvents = "none"; //a 태그
-
-    if (num === 50) {
-      clearInterval(ani);
-      panel.style.transform = "rotate(" + deg[setNum] + "deg)";
+  var baseDeg = 7200;
+  var stopDeg = baseDeg + deg[rRandom()];
+  panel.animate(
+    [
+      { transform: "rotate(0deg)" },
+      { transform: "rotate(" + stopDeg + "deg)" }
+    ],
+    {
+      fill: "forwards",
+      duration: 7000,
+      easing: "ease-out"
     }
-  }, 50);
-};
-
-const rLayerPopup = (num) => {
-  switch (num) {
-    case 1:
-      alert("당첨!! 스타벅스 아메리카노");
-      break;
-    case 3:
-      alert("당첨!! 햄버거 세트 교환권");
-      break;
-    case 5:
-      alert("당첨!! CU 3,000원 상품권");
-      break;
-    default:
-      alert("꽝!! 다음기회에");
-  }
-};
-
-const rReset = (ele) => {
-  setTimeout(() => {
-    ele.disabled = false;
-    ele.style.pointerEvents = "auto";
-    rLayerPopup(setNum);
-    hiddenInput.remove();
-  }, 5500);
+  );
 };
 
 document.addEventListener("click", function (e) {
   var target = e.target;
   if (target.tagName === "BUTTON") {
     rRotate();
-    rReset(target);
   }
 });
 
